df00943b21c5c428cfe036807b2ad411f634c60f
LironSif <lironsifado@gmail.com>
2023-09-18 12:26:45 +0300

    1.1

---
+++ Tasks 1-3/1.1.js
@@ -0,0 +1,26 @@
+// let card ={
+// let nameOf = "Liron"
+// let age = 7
+// let address = [
+//         street: "123 Main St",
+//         city: "Hometown",
+//         zip: 12345
+//     ]
+// let inTheCity = true
+// let alter = undefined
+// let symbol = Symbol('$');
+// } 
+let card = {
+    nameOf: "Liron",
+    age: 7,
+    address: {
+        street: "123 Main St",
+        city: "Hometown",
+        zip: 12345
+    },
+    inTheCity: true,
+    alter: undefined,
+    symbol: Symbol('$')
+};
+
+console.log(card);
