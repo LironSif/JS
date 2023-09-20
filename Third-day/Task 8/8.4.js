fcb75fbb9cef3b8f21a97c7ed271f9ee3e960d0c
LironSif <lironsifado@gmail.com>
2023-09-21 00:32:55 +0300

    8.4

---
+++ Third-day/Task 8/8.4.js
@@ -0,0 +1,25 @@
+const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
+function counter(arr){
+    let arrString = arr.join('')
+    const count = {}
+    let max = 0
+    let maxch 
+    for(let i = 0; i < arrString.length ; i++){
+
+        let ch = [arrString[i].toLowerCase()]
+
+        if(count[ch]){
+            count[ch] += 1
+        }else{
+            count[ch] = 1
+        }
+       if(count[ch] > max){
+        max = count[ch]
+        maxch = ch
+
+       }
+    }
+    return maxch
+}
+
+console.log(counter(array))
