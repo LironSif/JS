55cfb8b49e7339c41c3b919f9f3cc948e3538ff3
LironSif <lironsifado@gmail.com>
2023-09-21 11:21:47 +0300

    2

---
+++ Day 4/2.1.js
@@ -0,0 +1,8 @@
+let countryInfo = function(country, population ,capitalCity ){
+
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+console.log(countryInfo("Israel", 50, "Jerusalem"));
+console.log(countryInfo("japen", 200, "Tokyo"));
+console.log(countryInfo("cali", 100, "sacramanto"));
+
---
+++ Day 4/2.2.js
@@ -0,0 +1,4 @@
+let tellFortune = function (jobTitle,location,partnerName,numberOfChildren,){
+    return `You will be a ${jobTitle} in ${location} married to ${partnerName} with ${numberOfChildren} children`
+};
+console.log(tellFortune('programer' ,'new almonde' ,'dani', 5))
