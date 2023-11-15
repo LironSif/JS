f801b1854fb0ef977d444b824a6c7fd337af6738
LironSif <lironsifado@gmail.com>
2023-11-15 14:44:53 +0200

    day1.2

---
+++ Day 12/24.3.js
@@ -0,0 +1,21 @@
+
+Array.prototype.myFilter = function(callback) {
+    const filteredArray = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            filteredArray.push(this[i]);
+        }
+    }
+    return filteredArray;
+};
+
+
+Array.prototype.myFind = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            return this[i];
+        }
+    }
+};
+
+
